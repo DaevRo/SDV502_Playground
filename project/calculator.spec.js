eae837ca42b1daba1b7bf365ebf902330b0a74d4
Dave <david-rowe2@live.nmit.ac.nz>
2021-08-02 13:22:31 +1200

    updated calculator.spec

--- project/calculator.spec.js
+++ project/calculator.spec.js
@@ -1,46 +1,46 @@
-describe('calculator,js', funciton() {
-    it('should add numbers to total', function () {
+describe('calculator.js', function() {
+    it('should add numbers to the total', function() {
         const calculator = new Calculator()
         calculator.add(5)
-
+    
         expect(calculator.total).toBe(5)
-
+ 
     })
-
-    it('should subtract numbers from total', function () {
+ 
+    it('should subtract numbers from total', function() {
         const calculator = new Calculator()
         calculator.total = 30
         calculator.subtract(5)
-
+    
         expect(calculator.total).toBe(25)
     })
-
-    it('should multiply numbers to total', function () {
+    
+    it('should multiply numbers from total', function() {
         const calculator = new Calculator()
         calculator.total = 20
         calculator.multiply(5)
-
+    
         expect(calculator.total).toBe(100)
     })
-
-    it('should divide numbers to total', function () {
+ 
+    it('should divide numbers from total', function() {
         const calculator = new Calculator()
         calculator.total = 100
         calculator.divide(5)
-
+    
         expect(calculator.total).toBe(20)
     })
-
-    it('should throw an error if total is not a number', function() {
-        const calculator = new Calculator()
-
-        expect(calculator.total).toBe(0)
-        expect(calculator.total).toBeFalsy()
-    })
-    it('has constructor', function() {
-        const calculator = new Calculator()
-        const calculator2 = new Calculator()
-
-        expect(calculator).toEqual(calculator2)
-    })
+})
+ 
+it('should throw an error if total; is not a number', function () {
+    const calculator = new Calculator()
+ 
+    expect(calculator.total).toBe(0)
+    expect(calculator.total).toBeFalsy()
+})
+it ('has constructor', function () {
+    const calculator = new Calculator()
+    const calculator2 = new Calculator()
+ 
+    expect(calculator).toEqual(calculator2)
 })
